30b42972c121a8c0a6e9319ac9a64b90b451ac56
Shantanu Goel <shantanu.goel@gmail.com>
2017-09-14 22:14:53 +0530

    cosmetic

--- deecubes/deecubes.py
+++ deecubes/deecubes.py
@@ -3,27 +3,22 @@ import logging
 
 from deecubes.constants import VERSION
 
+
 def main():
   parser = argparse.ArgumentParser(prog='deecubes')
   parser.add_argument('-v', '--version', action='version',
                       version='%(prog)s version ' + VERSION)
   parser.add_argument('-l', '--log', type=int, action='store',
-                      help='Set log level. 0=> Warning, 1=>Info, 2=>Debug',
-                      default=0)
+                      help='Set log level. 0=> Warning, 1=>Info, 2=>Debug', default=0)
 
   action = parser.add_mutually_exclusive_group()
-  action.add_argument('-a', '--add', action='store',
-                      help='Generate shorturl for given link')
-  action.add_argument('-d', '--delete', action='store',
-                      help='Delete given shorturl')
-  action.add_argument('-s', '--sync', action='store_true', default=False,
-                      help='Sync raw data storage and html output')
+  action.add_argument('-a', '--add', action='store', help='Generate shorturl for given link')
+  action.add_argument('-d', '--delete', action='store', help='Delete given shorturl')
+  action.add_argument('-s', '--sync', action='store_true', default=False, help='Sync raw data storage and html output')
 
   req_args = parser.add_argument_group('required arguments')
-  req_args.add_argument('-r', '--raw-data-path', required=True,
-                        help='Raw data storage path')
-  req_args.add_argument('-o', '--output-path', required=True,
-                        help='HTML output path')
+  req_args.add_argument('-r', '--raw-data-path', required=True, help='Raw data storage path')
+  req_args.add_argument('-o', '--output-path', required=True, help='HTML output path')
 
   try:
     args = parser.parse_args()
@@ -36,8 +31,7 @@ def main():
     log_level = logging.INFO
   else:
     log_level = logging.WARNING
-  logging.basicConfig(level=log_level,
-                      format='%(asctime)s: %(filename)s - %(message)s')
+  logging.basicConfig(level=log_level, format='%(asctime)s: %(filename)s - %(message)s')
 
 
 if __name__ == "__main__":
