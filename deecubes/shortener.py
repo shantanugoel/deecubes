4ab54b4c6e4d1e42922f0c0cdb8e96d3ca413a41
Shantanu Goel <shantanu.goel@gmail.com>
2017-09-15 22:16:06 +0530

    working sync

--- deecubes/shortener.py
+++ deecubes/shortener.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import glob
 from binascii import crc32
 
 from deecubes.utils import base64_encode
@@ -18,18 +19,19 @@ class Shortener():
     self.raw_path = raw_path
     self.output_path = output_path
 
-  def _save(self, shorturl, url):
+  def _save_raw(self, shorturl, url):
     raw_file_name = shorturl + '.txt'
     # TODO: Handle conflicts while maintaining determinism
     raw_file = os.path.join(self.raw_path, raw_file_name)
+    logging.debug('Saving raw file %s' % (raw_file))
+    with open(raw_file, 'w') as f:
+      f.write(url)
+
+  def _save_output(self, shorturl, url):
     output_dir = os.path.join(self.output_path, shorturl)
     output_file = os.path.join(output_dir, 'index.html')
     preview_file = os.path.join(output_dir, 'preview.html')
     os.mkdir(output_dir)
-
-    logging.debug('Saving raw file %s' % (raw_file))
-    with open(raw_file, 'w') as f:
-      f.write(url)
 
     logging.debug('Saving output file %s' % (output_file))
     with open(output_file, 'w') as f:
@@ -46,7 +48,8 @@ class Shortener():
 
   def add(self, shorturl, url):
     logging.debug('Adding shorturl %s for %s' % (shorturl, url))
-    self._save(shorturl, url)
+    self._save_raw(shorturl, url)
+    self._save_output(shorturl, url)
 
   def generate(self, url):
     shorturl = self._encode(url)
@@ -54,4 +57,12 @@ class Shortener():
     self.add(shorturl, url)
 
   def sync(self):
-    pass
+    # TODO: Use asyncio to make this faster
+    search_pattern = os.path.join(self.raw_path, '*.txt')
+    raw_files = glob.glob(search_pattern)
+    for file in raw_files:
+      shorturl = os.path.splitext(os.path.basename(file))[0]
+      if not os.path.isdir(os.path.join(self.output_path, shorturl)):
+        with open(file, 'r') as f:
+          url = f.readline()
+        self._save_output(shorturl, url)
