4e1d6f9b8c4e9b9a91f0f963d1a33c10eebe0adc
Shantanu Goel <shantanu.goel@gmail.com>
2017-09-17 01:34:04 +0530

    0.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 
-version = '0.2.1'
+version = '0.2.2'
 
 setup(
   name='deecubes',
