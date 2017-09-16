b3e3c20d4a72194319a75abfcb045302f3ec7f39
Shantanu Goel <shantanu.goel@gmail.com>
2017-09-16 21:10:42 +0530

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 
-version = '0.1.0'
+version = '0.2.0'
 
 setup(
   name='deecubes',
